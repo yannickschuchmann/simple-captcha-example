fb0884f620e37179a170199a84b11e5d28918d75
Yannick Schuchmann <yannick.schuchmann@googlemail.com>
2015-07-12 20:04:07 +0200

    captcha api

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem 'sinatra'
+gem 'sinatra-synchrony'
+gem 'erubis'
+gem 'rake'
+gem 'moneta'
+gem 'byebug'
+gem 'random-word'
---
+++ app.rb
@@ -0,0 +1,29 @@
+require 'sinatra'
+require 'moneta'
+require 'securerandom'
+require 'byebug'
+require 'random-word'
+require 'sinatra/synchrony'
+
+register Sinatra::Synchrony
+use Rack::Session::Cookie, :secret => 'FURFM-SCHNAPS'
+
+store = Moneta.new(:Memory)
+ACCEPTED = "accepted_"
+
+get '/' do
+  user_id = session['user_id'] = session[:user_id] || SecureRandom.hex
+  if store.key?(ACCEPTED + user_id)
+    erb :secured
+  else
+    store[user_id] = RandomWord.adjs.next + " " + RandomWord.adjs.next unless store[user_id]
+    @phrase = store[user_id]
+    erb :input
+  end
+end
+
+get '/captcha' do
+  user_id = session[:user_id]
+  store[ACCEPTED + user_id] = true if store[user_id] == params[:captcha_input]
+  redirect to('/')
+end
